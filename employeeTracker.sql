37f3e37cc22c22b683e10b13e222c0a638e361f8
Francisco Rosado <Franciscorosado09@gmail.com>
2021-02-08 11:37:59 -0500

    setting up initial schema, server, package.json and gitignore.

---
+++ employeeTracker.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10, 2) NULL,
+  department_id INT default 1,
+  PRIMARY KEY (id)
+);
+
+
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SElECT * FROM roles;
+SELECT * FROM employee;
